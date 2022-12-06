9cfab685616f0d22b23284dbd90804a7f0da70af
Calin Michnea <113528510+CalinMichnea@users.noreply.github.com>
2022-12-06 13:29:13 +0100

    Delete Homeworks/Week 13 homework-Week 12 Updated directory

--- Homeworks/Week 13 homework-Week 12 Updated/sources/articles/ArticleItem/ArticleItem.model.js
+++
@@ -1,8 +0,0 @@
-function ArticleItemModel() {
-  Model.call(this);
-  this.templateUrl = "./sources/articles/ArticleItem/ArticleItem.template.html";
-
-  return this;
-}
-
-ArticleItemModel.prototype = Object.create(Model.prototype);
